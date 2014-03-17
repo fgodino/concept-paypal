29a0751eba5c28dc96afd88f1b3c0d904805910c
ailopera <lopera.martinez.ana.isabel@gmail.com>
2014-03-17 10:54:02 +0100

    utils.js added

---
+++ utils.js
@@ -0,0 +1,13 @@
+
+exports.replace = function(body){
+	var text = decodeURIComponent(body);
+	var patt1 = /\]\.[\w,\d]+\=/g;
+	var result = text.match(patt1);
+	var replaced = text;
+
+	for(var i = 0; i < result.length; i++){
+		var cad = result[i];
+		var toReplace = "][" + cad.substring(2,cad.length-1) + "]=";
+		var replaced = replaced.replace(cad, toReplace);
+	}
+}
