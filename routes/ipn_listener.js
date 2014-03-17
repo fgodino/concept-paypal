e1fd8bdbd284197f7b8d1830cd4c8027af3b327b
fernando <fernando@Lannister.(none)>
2014-03-17 10:04:06 +0100

    ipn

--- routes/ipn_listener.js
+++ routes/ipn_listener.js
@@ -1,29 +1,42 @@
 
 var ipn = require("../my-verify.js");
 var store = require("../store");
+var qs = require('qs');
 
 var notifications = {};
 
 exports.ipn_listener = function (req, res) {
 	res.send(200);
 
-	console.log(req.body);
+	var parsedBody;
 
 	ipn.verify(req.body, function (err, msg) {
 		if (err){
 			console.log(err, msg);
 		}
 		else{
+			var text = decodeURIComponent(req.body);
+			var patt1 = /\]\.[\w,\d]+\=/g;
+			var result = text.match(patt1);
+			var replaced = text;
+
+			for(var i = 0; i < result.length; i++){
+				var cad = result[i];
+				var toReplace = "][" + cad.substring(2,cad.length-1) + "]=";
+				var replaced = replaced.replace(cad, toReplace);
+			}
+
+			parsedBody = qs.parse(replaced);
 			if(req.body.status == 'COMPLETED'){
 				/* Hay que comprobar que el email pertenece a una cuenta de Paypal
 				 * (receiver)
 				 * Hay que comprobar que el id de la transacción no esté repetido
 				 * Verificar que el artículo se corresponde con el precio indicado */
-				var notification = req.query.item;
-				addNotification(notification);
+				 var notification = req.query.item;
+				 addNotification(notification);
+				}
 			}
-		}
-	});
+		});
 }
 
 exports.addNotification = function (notification) {
@@ -40,7 +53,7 @@ exports.getNotifications = function () {
 
 var Notification = function(notification){
 	console.log(notification);
-  var currentItem = notifications[notification.txn_id];
-  this.name = notification.item_name;
-  this.number = notification.item_number;
+	var currentItem = notifications[notification.txn_id];
+	this.name = notification.item_name;
+	this.number = notification.item_number;
 }
